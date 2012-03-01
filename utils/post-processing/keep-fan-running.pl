a790b707821d9de539a482e72ed474937a541c25
Michael Moon <triffid.hunter@gmail.com>
2012-03-01 21:39:12 +1100

    interim fix for #241, also specifically written to be a great skeleton/example post-process script

---
+++ utils/post-processing/keep-fan-running.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl -i
+
+use strict;
+use Getopt::Long qw(:config bundling pass_through);
+use XXX;
+
+# default values
+my $min_fan_speed = 35 * 255 / 100;
+my $layer_height = 0.2;
+
+# read specific options from command line
+GetOptions(
+	'min-fan-speed=i' => \$min_fan_speed,
+	'layer-height=f' => \$layer_height,
+);
+
+# read other options from command line
+my %options;
+while ($ARGV[0] =~ '^--') {
+    my ($arg, $value) = (shift, shift);
+    $arg =~ s/^--//;
+    $options{$arg} = $value;
+}
+
+# runtime variables
+my $fan_speed = $min_fan_speed * 255 / 100;
+my $past_start_gcode = 0;
+my $past_first_layer = 0;
+
+# now we start processing
+while (<>) {
+	# if we find a G90, we're probably looking at the start of the print proper, after user's start codes
+	if ($past_start_gcode == 0 && /G90\b/) {
+		$past_start_gcode = 1;
+	}
+	elsif ($past_start_gcode == 1) {
+		# set fan to idle after the first layer
+		if ($past_first_layer == 0 && /Z([\d\.]+)/) {
+			if ($1 > $layer_height) {
+				$past_first_layer = 1;
+				printf "M106 S%d\n", $fan_speed;
+			}
+		}
+		# when we get a non-maximum fan speed, save it
+		if (/M106\s+S(\d+)/) {
+			$fan_speed = $1
+				if $1 >= ($min_fan_speed * 255 / 100) && $1 < 255;
+		}
+		# replace all M107 with the previous fan speed
+		else {
+			s/\bM107\b/sprintf("M106 S%d",$fan_speed)/ei;
+		}
+	}
+	# write (possibly altered) line to output file
+	print;
+}
