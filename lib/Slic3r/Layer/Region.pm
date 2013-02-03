f18f2782fc0b29f091e020595555c70e58e654c0
Christopher Keller <cakeller98@gmail.com>
2013-02-03 04:13:03 -0800

    Finer Gap Filling until Medial Axis is used.

--- lib/Slic3r/Layer/Region.pm
+++ lib/Slic3r/Layer/Region.pm
@@ -290,7 +290,7 @@ sub make_perimeters {
             )};
             
             my $w = $self->perimeter_flow->width;
-            my @widths = (1.5 * $w, $w, 0.5 * $w);  # worth trying 0.2 too?
+            my @widths = (0.6 * $w, 0.3 * $w, 0.1 * $w);  # worth trying 0.2 too?
             foreach my $width (@widths) {
                 my $flow = $self->perimeter_flow->clone(width => $width);
                 
