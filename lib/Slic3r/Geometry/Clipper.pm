87e89e3971be95ee5dffc47e2c8956a4874483cf
Petr Ledvina <ledvinap@gmail.com>
2014-04-10 14:52:08 +0200

    export offset_pl function to perl. Function offsets polylines, returns polygons.

--- lib/Slic3r/Geometry/Clipper.pm
+++ lib/Slic3r/Geometry/Clipper.pm
@@ -4,7 +4,7 @@ use warnings;
 
 require Exporter;
 our @ISA = qw(Exporter);
-our @EXPORT_OK = qw(offset offset_ex
+our @EXPORT_OK = qw(offset offset_ex offset_pl
     diff_ex diff union_ex intersection_ex xor_ex JT_ROUND JT_MITER
     JT_SQUARE is_counter_clockwise union_pt offset2 offset2_ex
     intersection intersection_pl diff_pl union CLIPPER_OFFSET_SCALE
