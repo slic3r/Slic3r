3f73f135697005f9272ffea4425d812fb7147adb
Alessandro Ranellucci <aar@cpan.org>
2016-01-05 15:23:07 +0100

    Define aliases for event macros for legacy wxWidgets versions

--- xs/src/slic3r/GUI/wxinit.h
+++ xs/src/slic3r/GUI/wxinit.h
@@ -7,4 +7,14 @@
 // Perl redefines a _ macro, so we undef this one
 #undef _
 
+// legacy macros
+// https://wiki.wxwidgets.org/EventTypes_and_Event-Table_Macros
+#ifndef wxEVT_BUTTON
+#define wxEVT_BUTTON wxEVT_COMMAND_BUTTON_CLICKED
+#endif
+
+#ifndef wxEVT_HTML_LINK_CLICKED
+#define wxEVT_HTML_LINK_CLICKED wxEVT_COMMAND_HTML_LINK_CLICKED
+#endif
+
 #endif
