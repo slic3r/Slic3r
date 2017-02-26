80718b79a9c96fdeb764f32266917e2ffd4d2597
bubnikv <bubnikv@gmail.com>
2017-02-26 21:59:09 +0100

    AdMesh: unify positive and negative zeros in stl_check_facets_exact() and stl_check_facets_nearby() New function stl_transform() by a 3x4 matrix. Some constness improvements.
    Conflicts:
    
    	xs/src/admesh/stlinit.c

--- xs/src/admesh/stl.h
+++ xs/src/admesh/stl.h
@@ -197,6 +197,7 @@ extern void stl_rotate_z(stl_file *stl, float angle);
 extern void stl_mirror_xy(stl_file *stl);
 extern void stl_mirror_yz(stl_file *stl);
 extern void stl_mirror_xz(stl_file *stl);
+extern void stl_transform(stl_file *stl, float *trafo3x4);
 extern void stl_open_merge(stl_file *stl, char *file);
 extern void stl_invalidate_shared_vertices(stl_file *stl);
 extern void stl_generate_shared_vertices(stl_file *stl);
