62675a2b18bd30d14fb76971122e146be22c883e
Alessandro Ranellucci <aar@cpan.org>
2017-03-01 20:18:45 +0100

    Use Slic3r::IO::STL for reading STL files

--- lib/Slic3r/Format/STL.pm
+++ lib/Slic3r/Format/STL.pm
@@ -1,27 +1,12 @@
 package Slic3r::Format::STL;
 use Moo;
 
-use File::Basename qw(basename);
-
 sub read_file {
     my $self = shift;
     my ($file) = @_;
     
-    my $path = Slic3r::encode_path($file);
-    die "Failed to open $file\n" if !-e $path;
-    
-    my $mesh = Slic3r::TriangleMesh->new;
-    $mesh->ReadSTLFile($path);
-    $mesh->check_topology;
-    
-    die "This STL file couldn't be read because it's empty.\n"
-        if $mesh->facets_count == 0;
-    
     my $model = Slic3r::Model->new;
-    
-    my $basename = basename($file);
-    my $object = $model->add_object(input_file => $file, name => $basename);
-    my $volume = $object->add_volume(mesh => $mesh, name => $basename);
+    $model->read_stl($file);
     return $model;
 }
 
