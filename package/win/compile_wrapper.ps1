a591544d359be41cb9c58a3a1b4be3fd9a1131e4
Joseph Lenox <lenox.joseph@gmail.com>
2017-03-01 21:42:11 -0600

    set default perl version, added more linker options Added libgcc_s_sjlj-1.dll to pack list.

--- package/win/compile_wrapper.ps1
+++ package/win/compile_wrapper.ps1
@@ -1,7 +1,14 @@
 # Short Powershell script to build a wrapper exec
+if ($args[0])
+{
+	$perlver = $args[0]
+} else 
+{
+	$perlver = 518
+}
 
-$perlver = $args[0] 
 $perllib = "-lperl$perlver"
 Write-Host $perllib
 
-g++ -v -I'C:\strawberry\perl\lib\CORE\' -static-libgcc -static-libstdc++ $perllib -L'C:\strawberry\perl\bin\'  .\shell.cpp -o slic3r.exe | Write-Host
+g++ -v -I'C:\strawberry\perl\lib\CORE\' -static-libgcc -static-libstdc++ -L'C:\strawberry\c\lib' -L'C:\strawberry\perl\bin' -L'C:\strawberry\perl\lib\CORE\' $perllib .\shell.cpp -o slic3r.exe | Write-Host
+
