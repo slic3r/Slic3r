7355d395849ae3b6efe416712ce64d0ae7e270eb
Alessandro Ranellucci <aar@cpan.org>
2017-03-05 16:52:05 +0100

    Prevent race conditions when loading files into GUI from command line

--- slic3r.pl
+++ slic3r.pl
@@ -112,12 +112,14 @@ if ((!@ARGV || $opt{gui}) && !$opt{save} && eval "require Slic3r::GUI; 1") {
     }
     $gui = Slic3r::GUI->new;
     setlocale(LC_NUMERIC, 'C');
-    $gui->{mainframe}->load_config_file($_) for @{$opt{load}};
-    $gui->{mainframe}->load_config($cli_config);
-    foreach my $input_file (@ARGV) {
-        $input_file = Slic3r::decode_path($input_file);
-        $gui->{mainframe}{plater}->load_file($input_file) unless $opt{no_plater};
-    }
+    $gui->CallAfter(sub {
+        $gui->{mainframe}->load_config_file($_) for @{$opt{load}};
+        $gui->{mainframe}->load_config($cli_config);
+        foreach my $input_file (@ARGV) {
+            $input_file = Slic3r::decode_path($input_file);
+            $gui->{mainframe}{plater}->load_file($input_file) unless $opt{no_plater};
+        }
+    });
     $gui->MainLoop;
     exit;
 }
