ca73765ad7625cc5b4ade4802aa9efe77b32187e
Alessandro Ranellucci <aar@cpan.org>
2017-03-06 16:50:34 +0100

    Fixed typo in assert

--- xs/src/libslic3r/IO.cpp
+++ xs/src/libslic3r/IO.cpp
@@ -102,7 +102,7 @@ OBJ::read(std::string input_file, Model* model)
         std::vector<Point3> facets;
         
         // Read vertices.
-        assert((shape->mesh.vertices.size() % 3) == 0);
+        assert((attrib.vertices.size() % 3) == 0);
         for (size_t v = 0; v < attrib.vertices.size(); v += 3) {
             points.push_back(Pointf3(
                 attrib.vertices[v],
