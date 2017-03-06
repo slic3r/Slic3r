439daf15d38972ed3603fd95dbb2a6df52069c3e
Joseph Lenox <lenox.joseph@gmail.com>
2017-03-06 16:58:51 -0600

    Reversed logic for branch detection

--- package/osx/make_dmg.sh
+++ package/osx/make_dmg.sh
@@ -25,14 +25,14 @@ fi
 if [ -z ${GIT_BRANCH+x} ] && [ -z ${APPVEYOR_REPO_BRANCH+x} ]; then
     current_branch=$(git symbolic-ref HEAD | sed 's!refs\/heads\/!!')
 else
-    if [ -z ${GIT_BRANCH+x} ]; then
-        echo "Setting to APPVEYOR_REPO_BRANCH"
-        current_branch=$APPVEYOR_REPO_BRANCH
-    elif [ -z ${APPVEYOR_REPO_BRANCH+x} ]; then
-        current_branch="unknown"
-    else
+    current_branch="unknown"
+    if [ ! -z ${GIT_BRANCH+x} ]; then
         echo "Setting to GIT_BRANCH"
         current_branch=$(echo $GIT_BRANCH | cut -d / -f 2)
+    fi
+    if [ ! -z ${APPVEYOR_REPO_BRANCH+x} ]; then
+        echo "Setting to APPVEYOR_REPO_BRANCH"
+        current_branch=$APPVEYOR_REPO_BRANCH
     fi
 fi
 
