59f801d2ed29a8702a21391df7b5f7acb68a6e1d
Joseph Lenox <lordofhyphens@gmail.com>
2017-03-24 13:11:21 -0500

    added compile flag for FORCE_GUI

--- package/win/compile_wrapper.ps1
+++ package/win/compile_wrapper.ps1
@@ -11,5 +11,7 @@ $perllib = "-lperl$perlver"
 
 windres slic3r.rc -O coff -o slic3r.res
 g++ -c -I'C:\strawberry\perl\lib\CORE\' shell.cpp -o slic3r.o
+g++ -c -I'C:\strawberry\perl\lib\CORE\' -DFORCE_GUI shell.cpp -o slic3r-gui.o
 g++ -v -static-libgcc -static-libstdc++ -L'C:\strawberry\c\lib' -L'C:\strawberry\perl\bin' -L'C:\strawberry\perl\lib\CORE\' $perllib slic3r.o slic3r.res -o slic3r.exe | Write-Host
+g++ -v -static-libgcc -static-libstdc++ -L'C:\strawberry\c\lib' -L'C:\strawberry\perl\bin' -L'C:\strawberry\perl\lib\CORE\' $perllib slic3r-gui.o slic3r.res -o slic3r-gui.exe | Write-Host
 
