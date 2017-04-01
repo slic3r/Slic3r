5cfc97ada42057ff3a8f7210bac2b6886d341462
Joseph Lenox <lenox.joseph@gmail.com>
2017-04-01 18:05:18 -0500

    Point to my server and use the correct perlbrew path.

--- package/linux/travis-setup.sh
+++ package/linux/travis-setup.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 # Script to configure travis environment prior to build
-
 if [ ! -e $HOME/cache/slic3r-perlbrew-5.24.tar.bz2 ]; then
     curl -L "https://bintray.com/lordofhyphens/Slic3r/download_file?file_path=slic3r-perl.524.travis.tar.bz2" -o $HOME/cache/slic3r-perlbrew-5.24.tar.bz2; 
 fi
 
 if [ ! -e $HOME/cache/boost-compiled.tar.bz2 ]; then
-    curl -L "https://bintray.com/lordofhyphens/Slic3r/download_file?file_path=boost_1_63_0.built.gcc-4.9.4-buildserver.tar.bz2" -o $HOME/cache/boost-compiled.tar.bz2
+    curl -L "http://www.siusgs.com/slic3r/buildserver/boost_1_63_0.built.gcc-4.9.4-buildserver.tar.bz2" -o $HOME/cache/boost-compiled.tar.bz2
 fi
 
 if [ ! -e $HOME/cache/local-lib.tar.bz2 ]; then
-    curl -L "https://bintray.com/lordofhyphens/Slic3r/download_file?file_path=slic3r-dependencies.travis.tar.bz2" -o $HOME/cache/local-lib.tar.bz2
+    curl -L "http://www.siusgs.com/slic3r/buildserver/slic3r-dependencies.travis.tar.bz2" -o $HOME/cache/local-lib.tar.bz2
 fi
 
 tar -C$TRAVIS_BUILD_DIR -xjf $HOME/cache/local-lib.tar.bz2
-tar -C$HOME/perl5/perls -xjf $HOME/cache/slic3r-perlbrew-5.24.tar.bz2
+tar -C$HOME/perl5/perlbrew/perls -xjf $HOME/cache/slic3r-perlbrew-5.24.tar.bz2
 tar -C$HOME -xjf $HOME/cache/boost-compiled.tar.bz2
