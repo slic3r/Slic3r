fef9f659b1a5925cd96af63747c44349243c0681
Joseph Lenox <lenox.joseph@gmail.com>
2017-04-01 20:17:43 -0500

    added -v to install

--- package/linux/make_archive.sh
+++ package/linux/make_archive.sh
@@ -68,11 +68,11 @@ cp -fRP $SLIC3R_DIR/local-lib $archivefolder/local-lib
 cp -fRP $SLIC3R_DIR/lib/* $archivefolder/local-lib/lib/perl5/
 
 mkdir $archivefolder/bin
-echo "Symlinking libraries to root/bin..."
+echo "Symlinking libraries to $archivefolder/bin..."
 for bundle in $(find $archivefolder/local-lib/lib/perl5/*/auto/Wx -name '*.so') $(find $archivefolder/local-lib/lib/perl5/*/Alien/wxWidgets -name '*.so' -type f); do
     chmod +w $bundle
     for dylib in $(ldd $bundle | grep .so | grep local-lib | awk '{print $3}'); do
-	install $dylib $archivefolder/bin
+	install -v $dylib $archivefolder/bin
     done
 done
 
