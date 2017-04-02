4304c2a1f456a240b23e5e4f59b683b0f12b470b
Joseph Lenox <lenox.joseph@gmail.com>
2017-04-02 10:12:03 -0500

    fixed this time?

--- package/common/util.sh
+++ package/common/util.sh
@@ -75,8 +75,13 @@ function set_pr_id ()
     if [ ! -z ${APPVEYOR_PULL_REQUEST_NUMBER+x} ]; then
         PR_ID=$APPVEYOR_PULL_REQUEST_NUMBER
     fi
-    if [ ! -z ${TRAVIS_PULL_REQUEST_BRANCH+x} ] && [ "${TRAVIS_PULL_REQUEST_BRANCH}" != "false" ] ; then
+    if [ ! -z ${TRAVIS_PULL_REQUEST_BRANCH+x} ] && [ "${TRAVIS_PULL_REQUEST}" != "false" ] ; then
         PR_ID=$TRAVIS_PULL_REQUEST
+    fi
+    if [ ! -z ${PR_ID+x} ]; then
+        echo "Setting PR_ID to $PR_ID."
+    else 
+        echo "PR_ID remains unset."
     fi
 }
 
