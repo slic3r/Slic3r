f725b519e848694ea9ced856846161192b291d84
Joseph Lenox <lenox.joseph@gmail.com>
2017-04-03 22:12:13 -0500

    Fixed validator to properly only trigger on 0% or 0.

--- lib/Slic3r/Config.pm
+++ lib/Slic3r/Config.pm
@@ -271,8 +271,8 @@ sub validate {
     
     # support material
     if ($self->support_material) {
-        die "Angle value of 0 is illegal. Use some % value instead (e.g. 150%)\n"
-            if $self->support_material_threshold == 0 || $self->support_material_threshold eq '0';
+        die "Value of 0 is illegal. Use some % value instead (e.g. 150%) for auto.\n"
+            if $self->support_material_threshold =~ /^0+/;
     }
     
     
