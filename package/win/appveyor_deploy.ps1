f364f1290b2329dd2d48bbebb4638a4aa60d709b
Joseph Lenox <lenox.joseph@gmail.com>
2017-04-04 22:18:59 -0500

    converted key to PuTTY format

--- package/win/appveyor_deploy.ps1
+++ package/win/appveyor_deploy.ps1
@@ -3,7 +3,7 @@ cd package/win
 ./compile_wrapper.ps1 524 | Write-Output
 ./package_win32.ps1 524| Write-Output
 cd ../../
-& package/deploy/winscp.ps1 -dir win -key $BUILD_FOLDER/package/deploy/slic3r-upload.rsa.appveyor -file *.zip:
+& package/deploy/winscp.ps1 -dir win -key $BUILD_FOLDER/package/deploy/slic3r-upload.ppk.appveyor -file *.zip:
 
 if (!(Test-Path C:\project\slic3r\slic3r.par)) {
   Add-AppveyorCompilationMessage -Message "Failed to package!" -Category Error
