21aa301729b870ae11685b966c09613e3d40cbbc
Joseph Lenox <lenox.joseph@gmail.com>
2017-04-04 21:47:24 -0500

    working on script for winscp

--- package/deploy/winscp.ps1
+++ package/deploy/winscp.ps1
@@ -3,10 +3,10 @@
 #   UPLOAD_USER - user to upload to sftp server
 # KEY is assumed to be path to a ssh key for UPLOAD_USER
 
-param (
-    [parameter(Mandatory=$true, ParameterSetName="dir")] [string]$DIR,
-    [parameter(Mandatory=$true, ParameterSetName="key")] [string]$KEY,
-    [parameter(Mandatory=$true, ParameterSetName="file")] [string]$FILES
+Param(
+    [string]$DIR,
+    [string]$KEY,
+    [string]$FILE
 )
 UUSER=$env:UPLOAD_USER
-winscp.exe scp://$UUSER@dl.slic3r.org:$DIR /hostkey=* /privatekey=$KEY /upload $FILES
+winscp.exe scp://$UUSER@dl.slic3r.org:$DIR/ /hostkey=* /privatekey=$KEY /upload $FILES
