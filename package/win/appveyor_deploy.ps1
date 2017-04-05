a07dec03164f94af4e59dd7209e9a9fd338a669f
Joseph Lenox <lenox.joseph@gmail.com>
2017-04-04 23:47:48 -0500

    Redirect winscp output to local log to avoid leaking secret names.

--- package/win/appveyor_deploy.ps1
+++ package/win/appveyor_deploy.ps1
@@ -3,7 +3,8 @@ cd package/win
 ./compile_wrapper.ps1 524 | Write-Output
 ./package_win32.ps1 524| Write-Output
 cd ../../
-& package/deploy/winscp.ps1 -DIR win -KEY $BUILD_FOLDER/package/deploy/slic3r-upload.ppk -FILE *.zip:
+Add-AppveyorCompilationMessage -Message "Uploading to server."
+& ./package/deploy/winscp.ps1 -DIR win -KEY $env:APPVEYOR_BUILD_FOLDER/package/deploy/slic3r-upload.ppk -FILE *.zip *>> ./sftplog.txt
 
 if (!(Test-Path C:\project\slic3r\slic3r.par)) {
   Add-AppveyorCompilationMessage -Message "Failed to package!" -Category Error
