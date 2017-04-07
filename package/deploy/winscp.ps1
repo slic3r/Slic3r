650d61d35c1a6588a4f8baba1c22cec30a57962a
Joseph Lenox <lenox.joseph@gmail.com>
2017-04-06 19:57:05 -0500

    wrong or operator on powershell

--- package/deploy/winscp.ps1
+++ package/deploy/winscp.ps1
@@ -11,7 +11,7 @@ Param(
 Set-Variable -Name "UUSER" -Value "$env:UPLOAD_USER"
 Set-Variable -Name "UPLOAD" -Value "$($FILE | Resolve-Path)"
 if (Test-Path $KEY) {
-    if ($env:APPVEYOR_PULL_REQUEST_NUMBER || $env:APPVEYOR_REPO_BRANCH -ne "master" ) {
+    if ($env:APPVEYOR_PULL_REQUEST_NUMBER -Or $env:APPVEYOR_REPO_BRANCH -ne "master" ) {
         winscp.com /privatekey=$KEY /command "open sftp://$UUSER@dl.slic3r.org/$DIR/branches -hostkey=*" "put $UPLOAD ./$FILE" "exit"
     } else {
         winscp.com /privatekey=$KEY /command "open sftp://$UUSER@dl.slic3r.org/$DIR -hostkey=*" "put $UPLOAD ./$FILE" "exit"
