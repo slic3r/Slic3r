3776f1afc64d5bcf8079b4e83e0376a79f91573e
Alessandro Ranellucci <aar@cpan.org>
2017-04-15 11:10:53 +0200

    Fixed regression causing posPrepareInfill invalidation to have no effects. #3874

--- lib/Slic3r/Print/Object.pm
+++ lib/Slic3r/Print/Object.pm
@@ -156,13 +156,15 @@ sub prepare_infill {
     my ($self) = @_;
     
     return if $self->step_done(STEP_PREPARE_INFILL);
-    $self->set_step_started(STEP_PREPARE_INFILL);
     
     # This prepare_infill() is not really idempotent.
     # TODO: It should clear and regenerate fill_surfaces at every run 
     # instead of modifying it in place.
     $self->invalidate_step(STEP_PERIMETERS);
     $self->make_perimeters;
+    
+    # Do this after invalidating STEP_PERIMETERS because that would re-invalidate STEP_PREPARE_INFILL
+    $self->set_step_started(STEP_PREPARE_INFILL);
     
     # prerequisites
     $self->detect_surfaces_type;
