6e2ffaf4cae6e0d247c03fff084b9a4342b77452
Alessandro Ranellucci <aar@cpan.org>
2017-05-21 16:34:19 +0200

    Bugfix: first_layer_temperature was ignored by ooze_prevention for first layer skirt/brim. #3964

--- xs/src/libslic3r/GCode.cpp
+++ xs/src/libslic3r/GCode.cpp
@@ -119,7 +119,7 @@ OozePrevention::post_toolchange(GCode &gcodegen)
 int
 OozePrevention::_get_temp(GCode &gcodegen)
 {
-    return (gcodegen.layer != NULL && gcodegen.layer->id() == 0)
+    return gcodegen.first_layer
         ? gcodegen.config.first_layer_temperature.get_at(gcodegen.writer.extruder()->id)
         : gcodegen.config.temperature.get_at(gcodegen.writer.extruder()->id);
 }
