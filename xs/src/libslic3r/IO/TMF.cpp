f690cd8518b827331d67041b34bdc3269ba8f867
Samir55 <discere044@icloud.com>
2017-06-01 04:25:09 +0200

    Create the TMF Class.

--- xs/src/libslic3r/IO/TMF.cpp
+++ xs/src/libslic3r/IO/TMF.cpp
@@ -1,5 +1,18 @@
-#include "TMF.h"
+#include "../IO.hpp"
+#include <miniz/miniz.c>
 
+namespace Slic3r { namespace IO {
 
+// To be populated later
+static bool
+write(Model& model, std::string output_file){
+    return false;
+}
 
+// To be populated later
+static bool
+read(std::string input_file, Model* model){
+    return false;
+}
 
+} }
--- xs/src/libslic3r/IO/TMF.h
+++
@@ -1,6 +0,0 @@
-#ifndef SRC_3MF_H
-#define SRC_3MF_H
-
-#include <miniz/miniz.c>
-
-#endif //SRC_3MF_H
