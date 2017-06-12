3975d52d380ceafac0f4cbe175a27239370c8cc2
Caroline <cvrieous@gmail.com>
2017-06-12 17:57:18 -0500

    Created a C++ Wireframe utility
    -Compile it with CMake
    -WireframePrint now inherits from Print

---
+++ src/utils/wireframe.cpp
@@ -0,0 +1,25 @@
+#include "Config.hpp"
+#include "Geometry.hpp"
+#include "IO.hpp"
+#include "Model.hpp"
+#include "SLAPrint.hpp"
+#include "TriangleMesh.hpp"
+#include "libslic3r.h"
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <math.h>
+#include <boost/filesystem.hpp>
+#include <boost/nowide/args.hpp>
+#include <boost/nowide/iostream.hpp>
+
+
+using namespace Slic3r;
+
+int
+main(int argc, char **argv)
+{
+	
+	return 0;
+}
--- xs/src/libslic3r/WireframePrint.hpp
+++ xs/src/libslic3r/WireframePrint.hpp
@@ -1,11 +1,12 @@
 #ifndef slic3r_WireframePrint_hpp_
 #define slic3r_WireframePrint_hpp_
 
+#include "Print.hpp"
 #include "libslic3r.h"
 
 namespace Slic3r {
 
-class WireframePrint
+class WireframePrint : public Print
 {
 	
 };
