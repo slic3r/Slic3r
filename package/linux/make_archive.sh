4c958d24eed6ffad320bce993d3c32f516b4e01a
Joseph Lenox <lenox.joseph@gmail.com>
2017-06-21 22:21:27 -0500

    Refactor out the perl packages into a common file.

--- package/linux/make_archive.sh
+++ package/linux/make_archive.sh
@@ -92,15 +92,10 @@ cp -f $WD/startup_script.sh $archivefolder/$appname
 chmod +x $archivefolder/$appname
 
 echo "Copying perl from $PERL_BIN"
+# Edit package/common/coreperl to add/remove core Perl modules added to this package, one per line.
 cp -f $PERL_BIN $archivefolder/perl-local
 ${PP_BIN} wxextension .0 \
-	  -M attributes -M base -M bytes -M B -M POSIX \
-          -M FindBin -M Unicode::Normalize -M Tie::Handle \
-          -M Time::Local -M Math::Trig -M IO::Socket -M Errno \
-          -M Storable \
-          -M lib -M overload \
-          -M warnings -M local::lib \
-          -M strict -M utf8 -M parent \
+	  -M $(grep -v "^#" ${WD}/../common/coreperl | xargs | awk 'BEGIN { OFS=" -M "}; {$1=$1; print $0}') \
           -B -p -e "print 123" -o $WD/_tmp/test.par
 unzip -qq -o $WD/_tmp/test.par -d $WD/_tmp/
 cp -rf $WD/_tmp/lib/* $archivefolder/local-lib/lib/perl5/
