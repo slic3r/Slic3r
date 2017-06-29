e6a2e94857c1867d263ee2a7b51c4a2bb927f0ae
Caroline <cvrieous@gmail.com>
2017-06-29 16:02:05 -0500

    WPRegion is filled out with dummy methods

--- xs/src/libslic3r/WireframePrint.hpp
+++ xs/src/libslic3r/WireframePrint.hpp
@@ -3,13 +3,28 @@
 
 #include "Print.hpp"
 #include "libslic3r.h"
+#include "PrintConfig.hpp"
 
 namespace Slic3r {
 
+class WireframePrint;
+class WireframePrintObject;
+
 class WireframePrintRegion
 {
+	friend class WireframePrint;
+
 public:
-	WireframePrintRegion();
+	WireframePrintRegionConfig config;
+
+    WireframePrint* print();
+    //Flow flow(FlowRole role, double layer_height, bool bridge, bool first_layer, double width, const WireframePrintObject &object) const;
+    bool invalidate_state_by_config(const PrintConfigBase &config);
+
+    private:
+    WireframePrint* _print;
+    
+    WireframePrintRegion(WireframePrint* print);
 	~WireframePrintRegion();
 	
 };
--- xs/src/libslic3r/WireframePrintRegion.cpp
+++ xs/src/libslic3r/WireframePrintRegion.cpp
@@ -3,4 +3,27 @@
 namespace Slic3r
 {
 	
+WireframePrintRegion::WireframePrintRegion(WireframePrint* print)
+    : _print(print)
+{
+}
+
+WireframePrintRegion::~WireframePrintRegion()
+{
+}
+
+WireframePrint*
+WireframePrintRegion::print()
+{
+    return this->_print;
+}
+
+}
+
+bool
+WireframePrintRegion::invalidate_state_by_config(const PrintConfigBase &config)
+{
+
+	return false;
+
 }
