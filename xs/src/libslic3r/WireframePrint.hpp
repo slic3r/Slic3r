41f1e05c94f50bedccb51f4730c92df825170d60
Caroline <cvrieous@gmail.com>
2017-07-26 12:41:02 -0500

    WIP; Adding WPObject
    -Filled out the WPObject class similar to PrintObject

--- xs/src/libslic3r/WireframePrint.hpp
+++ xs/src/libslic3r/WireframePrint.hpp
@@ -4,6 +4,8 @@
 #include "Print.hpp"
 #include "libslic3r.h"
 #include "PrintConfig.hpp"
+#include "Model.hpp"
+#include "BoundingBox.hpp"
 
 namespace Slic3r {
 
@@ -31,8 +33,43 @@ public:
 
 class WireframePrintObject
 {
+	friend class WireframePrint;
+
 public:
-	WireframePrintObject();
+	WireframePrintObjectConfig config;
+
+	Point3 size;           // XYZ in scaled coordinates
+
+	LayerPtrs layers;
+	PrintState<PrintObjectStep> state;
+
+	WireframePrint* print();
+    ModelObject* model_object() { return this->_model_object; };
+
+    BoundingBox bounding_box() const;
+    std::set<size_t> extruders() const;
+
+    size_t total_layer_count() const;
+    size_t layer_count() const;
+    void clear_layers();
+    Layer* get_layer(int idx) { return this->layers.at(idx); };
+    const Layer* get_layer(int idx) const { return this->layers.at(idx); };
+    // print_z: top of the layer; slice_z: center of the layer.
+    Layer* add_layer(int id, coordf_t height, coordf_t print_z, coordf_t slice_z);
+    void delete_layer(int idx);
+
+    // methods for handling state
+    bool invalidate_state_by_config(const PrintConfigBase &config);
+    bool invalidate_step(PrintObjectStep step);
+    bool invalidate_all_steps();
+
+    void _slice();
+
+    private:
+    Print* _print;
+    ModelObject* _model_object;
+
+	WireframePrintObject(WireframePrint* print, ModelObject* model_object, const BoundingBoxf3 &modobj_bbox);
 	~WireframePrintObject();
 	
 };
