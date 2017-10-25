159ae6648bb0d766bfa99630cee4c6c7dae58ace
Joseph Lenox <lenox.joseph@gmail.com>
2017-10-24 19:06:40 -0500

    undef random

--- xs/src/xsinit.h
+++ xs/src/xsinit.h
@@ -28,7 +28,7 @@
 #undef sendto
 
 // undef macro that is causing problems on travis
-#undef random
+#undef seed
 
 // these need to be included early for Win32 (listing it in Build.PL is not enough)
 #include <ostream>
