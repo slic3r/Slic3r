157bc1224ad677c4f54517ff71e1e86e76e3854e
rmx-it <rmx-it@users.noreply.github.com>
2017-11-15 18:05:55 +0100

    Fix compiling for FreeBSD
    Fix compiling at least for FreeBSD 11+

--- xs/src/admesh/portable_endian.h
+++ xs/src/admesh/portable_endian.h
@@ -46,11 +46,11 @@
 #	define __LITTLE_ENDIAN LITTLE_ENDIAN
 #	define __PDP_ENDIAN    PDP_ENDIAN
 
-#elif defined(__OpenBSD__)
+#elif defined(__OpenBSD__) || defined(__FreeBSD__)
 
 #	include <sys/endian.h>
 
-#elif defined(__NetBSD__) || defined(__FreeBSD__) || defined(__DragonFly__)
+#elif defined(__NetBSD__) || defined(__DragonFly__)
 
 #	include <sys/endian.h>
 
