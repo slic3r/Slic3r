6c510cd2d6986b9aea64d8111a0c5c0ef7d4bc77
Florens Wasserfall <wasserfall@kalanka.de>
2017-12-21 13:35:06 +0100

    Removed factor 4 from layer height gradation, don't apply min/max layer height limitation after gradation. Fixes #4235

--- xs/src/libslic3r/PrintConfig.cpp
+++ xs/src/libslic3r/PrintConfig.cpp
@@ -1671,7 +1671,7 @@ PrintConfigDef::PrintConfigDef()
 
     def = this->add("z_steps_per_mm", coFloat);
     def->label = "Z full steps/mm";
-    def->tooltip = "Set this to the number of *full* steps (not microsteps) needed for moving the Z axis by 1mm; you can calculate this by dividing the number of microsteps configured in your firmware by the microstepping amount (8, 16, 32). Slic3r will round your configured layer height to the nearest multiple of that value in order to ensure the best accuracy. This is most useful for machines with imperial leadscrews or belt-driven Z or for unusual layer heights with metric leadscrews. Set to zero to disable this experimental feature.";
+    def->tooltip = "Set this to the number of *full* steps (not microsteps) needed for moving the Z axis by 1mm; you can calculate this by dividing the number of microsteps configured in your firmware by the microstepping amount (8, 16, 32). Slic3r will round your configured layer height to the nearest multiple of that value in order to ensure the best accuracy. This is most useful for machines with imperial leadscrews or belt-driven Z or for unusual layer heights with metric leadscrews. If you still experience wobbling, try using 4 * full_step to achieve the same motor phase pattern for every layer (important for belt driven z-axis). Set to zero to disable this experimental feature.";
     def->cli = "z-steps-per-mm=f";
     def->default_value = new ConfigOptionFloat(0);
 
