560c2b380e518b20ceecf0205021b174d7130732
Joseph Lenox <lordofhyphens@gmail.com>
2018-02-24 17:53:29 -0600

    Use doxygen comments.

--- xs/src/libslic3r/Fill/FillGyroid.hpp
+++ xs/src/libslic3r/Fill/FillGyroid.hpp
@@ -17,12 +17,12 @@ public:
     virtual Fill* clone() const { return new FillGyroid(*this); };
     virtual ~FillGyroid() {}
 
-    // require bridge flow since most of this pattern hangs in air
+    /// require bridge flow since most of this pattern hangs in air
     virtual bool use_bridge_flow() const { return true; }
 
 protected:
     
-    // mult of density, to have a good %of weight for each density parameter
+    /// mult of density, to have a good %of weight for each density parameter
     float scaling;
 
 
@@ -32,16 +32,16 @@ protected:
         ExPolygon                       &expolygon, 
         Polylines                       *polylines_out);
     
-    // create the gyroid grid to clip.
+    /// create the gyroid grid to clip.
     Polylines makeGrid(coord_t gridZ, double density, double layer_width, size_t gridWidth, size_t gridHeight, size_t curveType);
-    //add line poly in reverse if needed into array
+    ///add line poly in reverse if needed into array
     inline void correctOrderAndAdd(const int num, Polyline poly, Polylines &array);
-    //create a curved horinzontal line  (for each x, compute y)
+    ///create a curved horinzontal line  (for each x, compute y)
     Polyline makeLineHori(double xPos, double yPos, double width, double height, 
         double currentYBegin, double segmentSize, coord_t scaleFactor, 
         double zCs, double zSn, 
         bool flip, double decal=0);
-    //create a curved vertival line (for each y, compute x)
+    ///create a curved vertival line (for each y, compute x)
     Polyline makeLineVert(double xPos, double yPos, double width, double height, 
         double currentXBegin, double segmentSize, coord_t scaleFactor, 
         double zCs, double zSn, 
