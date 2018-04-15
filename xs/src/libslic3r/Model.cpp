0d257a5cd453c8674f4d5020fecc6b27cbc75f76
Oekn5w <38046255+Oekn5w@users.noreply.github.com>
2018-04-15 19:08:49 +0200

    Implementation of added variables in (new ModelVolume(*)) funcion

--- xs/src/libslic3r/Model.cpp
+++ xs/src/libslic3r/Model.cpp
@@ -936,12 +936,18 @@ ModelObject::print_info() const
 
 
 ModelVolume::ModelVolume(ModelObject* object, const TriangleMesh &mesh)
-:   mesh(mesh), modifier(false), object(object)
+:   mesh(mesh), modifier(false), input_file(""), object(object)
 {}
 
 ModelVolume::ModelVolume(ModelObject* object, const ModelVolume &other)
-:   name(other.name), mesh(other.mesh), config(other.config),
-    modifier(other.modifier), object(object)
+:   name(other.name),
+    mesh(other.mesh),
+    config(other.config),
+    modifier(other.modifier),
+    input_file(other.input_file),
+    input_file_obj_idx(other.input_file_obj_idx),
+    input_file_vol_idx(other.input_file_vol_idx),
+    object(object)
 {
     this->material_id(other.material_id());
 }
