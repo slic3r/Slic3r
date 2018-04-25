221432d2eadea311d8f4cc22cf17a455308ce28e
Joseph Lenox <lenox.joseph@gmail.com>
2018-04-24 22:46:26 -0500

    stub out more menus

--- src/GUI/GUI.hpp
+++ src/GUI/GUI.hpp
@@ -3,12 +3,25 @@
 #include "MainFrame.hpp"
 
 namespace Slic3r { namespace GUI {
+
+enum class PresetID {
+    PRINT = 0,
+    FILAMENT = 1,
+    PRINTER = 2
+};
+
 class App: public wxApp
 {
-    std::shared_ptr<Settings> gui_config; // GUI-specific configuration options
 public:
     virtual bool OnInit();
     App(std::shared_ptr<Settings> config) : wxApp(), gui_config(config) {}
+
+    void check_version(bool manual) { /* stub */}
+
+private:
+    std::shared_ptr<Settings> gui_config; // GUI-specific configuration options
+    Notifier* notifier;
+    
 };
 
 }} // namespace Slic3r::GUI
