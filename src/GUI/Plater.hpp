33242c4ba777d2e07c7c8ad6fc3e2d6b37646e0e
Joseph Lenox <lenox.joseph@gmail.com>
2018-04-29 10:13:39 -0500

    Stubbed Plater constructor.

--- src/GUI/Plater.hpp
+++ src/GUI/Plater.hpp
@@ -5,7 +5,19 @@
     #include <wx/wx.h>
 #endif
 
+#include <wx/notebook.h>
+
+#include <stack>
+
+#include "libslic3r.h"
+#include "Model.hpp"
+#include "Print.hpp"
+
 namespace Slic3r { namespace GUI {
+
+using UndoOperation = int;
+
+class Plater2DObject;
 
 class Plater : public wxPanel 
 {
@@ -13,6 +25,28 @@ public:
     Plater(wxWindow* parent, const wxString& title);
     void add();
 
+private:
+    Print print;
+    Model model;
+
+    bool processed {false};
+
+    std::vector<Plater2DObject> objects;
+
+    std::stack<UndoOperation> undo {}; 
+    std::stack<UndoOperation> redo {}; 
+
+    wxNotebook* preview_notebook {new wxNotebook(this, -1, wxDefaultPosition, wxSize(335,335), wxNB_BOTTOM)};
+
+};
+
+
+// 2D Preview of an object
+class Plater2DObject {
+public:
+    std::string name {""};
+    std::string identifier {""};
+    bool selected {false};
 };
 
 } } // Namespace Slic3r::GUI
