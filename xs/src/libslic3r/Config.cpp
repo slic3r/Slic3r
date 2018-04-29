d02516d8fed66b357e57d5f10120ed2669cfc771
Joseph Lenox <lenox.joseph@gmail.com>
2018-04-29 12:14:03 -0500

    Added factory function for new_from_defaults. Stubbed out new_from_cli.

---
+++ xs/src/libslic3r/Config.cpp
@@ -0,0 +1,31 @@
+#include "Config.hpp"
+#include "Log.hpp"
+
+namespace Slic3r {
+
+std::shared_ptr<Config> 
+Config::new_from_defaults() 
+{ 
+    return std::make_shared<Config>();
+}
+std::shared_ptr<Config> 
+Config::new_from_defaults(std::initializer_list<std::string> init)
+{
+    auto my_config(std::make_shared<Config>());
+    for (auto& opt_key : init) {
+        if (print_config_def.has(opt_key)) {
+            const std::string value { print_config_def.get(opt_key)->default_value->serialize() };
+            my_config->set_deserialize(opt_key, value);
+        }
+    }
+
+    return my_config;
+}
+
+std::shared_ptr<Config> 
+new_from_cli(const int& argc, const char* argv[])
+{
+    return std::make_shared<Config>();
+}
+
+} // namespace Slic3r
--- xs/src/libslic3r/Config.hpp
+++ xs/src/libslic3r/Config.hpp
@@ -1,19 +1,21 @@
 #ifndef CONFIG_HPP
 #define CONFIG_HPP
 
+#include <initializer_list>
+#include <memory>
+
 #include "PrintConfig.hpp"
 
 namespace Slic3r {
 
 class Config : DynamicPrintConfig {
 public:
-    static Config *new_from_defaults();
-    static Config *new_from_cli(const &argc, const char* argv[]);
+    static std::shared_ptr<Config> new_from_defaults();
+    static std::shared_ptr<Config> new_from_defaults(std::initializer_list<std::string> init);
+    static std::shared_ptr<Config> new_from_cli(const int& argc, const char* argv[]);
 
     void write_ini(const std::string& file) { save(file); }
     void read_ini(const std::string& file) { load(file); }
-
-
 };
 
 } // namespace Slic3r
