1548066318e52c86c832d168a3e3398f8d2a7766
Joseph Lenox <lenox.joseph@gmail.com>
2018-04-29 18:02:48 -0500

    Staged the 2D plater on its tab.

--- src/GUI/Plater/Plate2D.hpp
+++ src/GUI/Plater/Plate2D.hpp
@@ -4,11 +4,16 @@
 #ifndef WX_PRECOMP
     #include <wx/wx.h>
 #endif
+
 #include <vector>
 #include "Plater.hpp"
 #include "ColorScheme.hpp"
 #include "Settings.hpp"
 #include "Plater/Plater2DObject.hpp"
+#include "misc_ui.hpp"
+
+
+#include "Log.hpp"
 
 
 namespace Slic3r { namespace GUI {
@@ -22,7 +27,33 @@ private:
     std::shared_ptr<Slic3r::Config> config;
     std::shared_ptr<Settings> settings;
 
+    // Different brushes to draw with
     wxBrush objects_brush {};
+    wxBrush instance_brush {};
+    wxBrush selected_brush {};
+    wxBrush bed_brush {};
+    wxBrush dragged_brush {};
+    wxBrush transparent_brush {};
+
+    wxPen grid_pen {};
+    wxPen print_center_pen {};
+    wxPen clearance_pen {};
+    wxPen skirt_pen {};
+    wxPen dark_pen {};
+
+    bool user_drawn_background {(the_os == OS::Mac ? false : true)};
+    Plater2DObject selected_instance;
+
+    /// Handle mouse-move events
+    void mouse_drag(wxMouseEvent& e);
+
+    /// Handle repaint events
+    void repaint(wxPaintEvent& e);
+
+    /// Set/Update all of the colors used by the various brushes in the panel.
+    void set_colors();
+    
+    const std::string LogChannel {"GUI_2D"};
 };
 
 } } // Namespace Slic3r::GUI
