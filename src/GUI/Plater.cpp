e1667825a567b54cdc1c188196a438fe09b69e49
Joseph Lenox <lenox.joseph@gmail.com>
2018-04-28 23:37:27 -0500

    Moved plater stubs to its own cpp

---
+++ src/GUI/Plater.cpp
@@ -0,0 +1,13 @@
+#include "Plater.hpp"
+
+namespace Slic3r { namespace GUI {
+
+Plater::Plater(wxWindow* parent, const wxString& title) : 
+    wxPanel(parent, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL, title) { }
+void Plater::add() {
+
+}
+
+
+}} // Namespace Slic3r::GUI
+
