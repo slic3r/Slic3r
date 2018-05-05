ee6e897df1c8e83d49c53f666fc31d364c97e532
Joseph Lenox <lenox.joseph@gmail.com>
2018-05-05 17:36:20 -0500

    Added generic error message to Log; print error type to console.

--- xs/src/libslic3r/Log.hpp
+++ xs/src/libslic3r/Log.hpp
@@ -8,17 +8,21 @@ namespace Slic3r {
 class Log {
 public:
     static void fatal_error(std::string topic, std::wstring message) {
-        std::cerr << topic << ": ";
+        std::cerr << topic << " FERR" << ": ";
+        std::wcerr << message << std::endl;
+    }
+    static void error(std::string topic, std::wstring message) {
+        std::cerr << topic << "  ERR" << ": ";
         std::wcerr << message << std::endl;
     }
 
     static void info(std::string topic, std::wstring message) {
-        std::clog << topic << ": ";
+        std::clog << topic << " INFO" << ": ";
         std::wclog << message << std::endl;
     }
 
     static void warn(std::string topic, std::wstring message) {
-        std::cerr << topic << ": ";
+        std::cerr << topic << " WARN" << ": ";
         std::wcerr << message << std::endl;
     }
 
