deb0cfb27302f1c043a3113160faead19fc0a7bf
Joseph Lenox <lenox.joseph@gmail.com>
2018-05-06 22:43:02 -0500

    Added bare-bones stubs for 3D plater and 3D preview.

---
+++ src/GUI/Plater/Plate3D.hpp
@@ -0,0 +1,16 @@
+#ifndef PLATE3D_HPP
+#define PLATE3D_HPP
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+    #include <wx/wx.h>
+#endif
+
+namespace Slic3r { namespace GUI {
+
+class Plate3D : public wxPanel {
+public:
+    void update() {};
+};
+
+} } // Namespace Slic3r::GUI
+#endif
