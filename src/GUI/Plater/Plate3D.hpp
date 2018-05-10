375663c381173e03017b2a9b922f4609a9cfa2e3
Joseph Lenox <lenox.joseph@gmail.com>
2018-05-09 23:03:06 -0500

    Stubbed out classes for 3D plater/preview, 2D preview (toolpaths), and DLP/SLA preview tabs.

--- src/GUI/Plater/Plate3D.hpp
+++ src/GUI/Plater/Plate3D.hpp
@@ -4,12 +4,23 @@
 #ifndef WX_PRECOMP
     #include <wx/wx.h>
 #endif
+#include "Settings.hpp"
+#include "Model.hpp"
+#include "Config.hpp"
 
 namespace Slic3r { namespace GUI {
 
 class Plate3D : public wxPanel {
 public:
     void update() {};
+    Plate3D(wxWindow* parent, const wxSize& size, std::vector<PlaterObject>& _objects, std::shared_ptr<Model> _model, std::shared_ptr<Config> _config, std::shared_ptr<Settings> _settings) :
+    wxPanel(parent, wxID_ANY, wxDefaultPosition, size, wxTAB_TRAVERSAL), objects(_objects), model(_model), config(_config), settings(_settings)
+    {}
+private:
+    std::vector<PlaterObject>& objects; //< reference to parent vector
+    std::shared_ptr<Slic3r::Model> model;
+    std::shared_ptr<Slic3r::Config> config;
+    std::shared_ptr<Settings> settings;
 };
 
 } } // Namespace Slic3r::GUI
--- src/GUI/Plater/PreviewDLP.hpp
+++ src/GUI/Plater/PreviewDLP.hpp
@@ -1,5 +1,5 @@
-#ifndef PREVIEW3D_HPP
-#define PREVIEW3D_HPP
+#ifndef PREVIEWDLP_HPP
+#define PREVIEWDLP_HPP
 #include <wx/wxprec.h>
 #ifndef WX_PRECOMP
     #include <wx/wx.h>
@@ -11,10 +11,10 @@
 
 namespace Slic3r { namespace GUI {
 
-class Preview3D : public wxPanel {
+class PreviewDLP : public wxPanel {
 public:
     void reload_print() {};
-    Preview3D(wxWindow* parent, const wxSize& size, std::vector<PlaterObject>& _objects, std::shared_ptr<Model> _model, std::shared_ptr<Config> _config, std::shared_ptr<Settings> _settings) :
+    PreviewDLP(wxWindow* parent, const wxSize& size, std::vector<PlaterObject>& _objects, std::shared_ptr<Model> _model, std::shared_ptr<Config> _config, std::shared_ptr<Settings> _settings) :
     wxPanel(parent, wxID_ANY, wxDefaultPosition, size, wxTAB_TRAVERSAL), objects(_objects), model(_model), config(_config), settings(_settings)
     {}
 private:
