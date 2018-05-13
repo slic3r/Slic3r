212e2af210837fe0f322275b3a9a164c6b100a75
Joseph Lenox <lenox.joseph@gmail.com>
2018-05-13 13:55:53 -0500

    Convert colors to base-255

--- src/GUI/ColorScheme/Solarized.hpp
+++ src/GUI/ColorScheme/Solarized.hpp
@@ -20,22 +20,22 @@ public:
 
 
 private:
-    const wxColour COLOR_BASE0    {wxColour(0.51373,0.58039,0.58824)};
-    const wxColour COLOR_BASE00   {wxColour(0.39608,0.48235,0.51373)};
-    const wxColour COLOR_BASE01   {wxColour(0.34510,0.43137,0.45882)};
-    const wxColour COLOR_BASE02   {wxColour(0.02745,0.21176,0.25882)};
-    const wxColour COLOR_BASE03   {wxColour(0.00000,0.16863,0.21176)};
-    const wxColour COLOR_BASE1    {wxColour(0.57647,0.63137,0.63137)};
-    const wxColour COLOR_BASE2    {wxColour(0.93333,0.90980,0.83529)};
-    const wxColour COLOR_BASE3    {wxColour(0.99216,0.96471,0.89020)};
-    const wxColour COLOR_BLUE     {wxColour(0.14902,0.54510,0.82353)};
-    const wxColour COLOR_CYAN     {wxColour(0.16471,0.63137,0.59608)};
-    const wxColour COLOR_GREEN    {wxColour(0.52157,0.60000,0.00000)};
-    const wxColour COLOR_MAGENTA  {wxColour(0.82745,0.21176,0.50980)};
-    const wxColour COLOR_ORANGE   {wxColour(0.79608,0.29412,0.08627)};
-    const wxColour COLOR_RED      {wxColour(0.86275,0.19608,0.18431)};
-    const wxColour COLOR_VIOLET   {wxColour(0.42353,0.44314,0.76863)};
-    const wxColour COLOR_YELLOW   {wxColour(0.70980,0.53725,0.00000)};
+    const wxColour COLOR_BASE0    {wxColour(255*0.51373,255*0.58039,255*0.58824)};
+    const wxColour COLOR_BASE00   {wxColour(255*0.39608,255*0.48235,255*0.51373)};
+    const wxColour COLOR_BASE01   {wxColour(255*0.34510,255*0.43137,255*0.45882)};
+    const wxColour COLOR_BASE02   {wxColour(255*0.02745,255*0.21176,255*0.25882)};
+    const wxColour COLOR_BASE03   {wxColour(255*0.00000,255*0.16863,255*0.21176)};
+    const wxColour COLOR_BASE1    {wxColour(255*0.57647,255*0.63137,255*0.63137)};
+    const wxColour COLOR_BASE2    {wxColour(255*0.93333,255*0.90980,255*0.83529)};
+    const wxColour COLOR_BASE3    {wxColour(255*0.99216,255*0.96471,255*0.89020)};
+    const wxColour COLOR_BLUE     {wxColour(255*0.14902,255*0.54510,255*0.82353)};
+    const wxColour COLOR_CYAN     {wxColour(255*0.16471,255*0.63137,255*0.59608)};
+    const wxColour COLOR_GREEN    {wxColour(255*0.52157,255*0.60000,255*0.00000)};
+    const wxColour COLOR_MAGENTA  {wxColour(255*0.82745,255*0.21176,255*0.50980)};
+    const wxColour COLOR_ORANGE   {wxColour(255*0.79608,255*0.29412,255*0.08627)};
+    const wxColour COLOR_RED      {wxColour(255*0.86275,255*0.19608,255*0.18431)};
+    const wxColour COLOR_VIOLET   {wxColour(255*0.42353,255*0.44314,255*0.76863)};
+    const wxColour COLOR_YELLOW   {wxColour(255*0.70980,255*0.53725,255*0.00000)};
 
 };
 }} // namespace Slic3r::GUI
