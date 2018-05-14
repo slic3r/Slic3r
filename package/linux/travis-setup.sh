568eb792a0dacc54d94a0f9e6b57904c039d4ee0
Joseph Lenox <lenox.joseph@gmail.com>
2018-05-13 23:14:28 -0500

    ...

--- package/linux/travis-setup.sh
+++ package/linux/travis-setup.sh
@@ -11,12 +11,13 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
         fi
         tar -C$HOME -xjf $CACHE/wx${WXVERSION}.tar.bz2
     fi
-        if [ ! -e $CACHE/boost-compiled.tar.bz2 ]; then
-            echo "Downloading http://www.siusgs.com/slic3r/buildserver/boost_1_63_0.built.gcc-4.9.4-buildserver.tar.bz2 => $CACHE/boost-compiled.tar.bz2"
-            curl -L "http://www.siusgs.com/slic3r/buildserver/boost_1_63_0.built.gcc-4.9.4-buildserver.tar.bz2" -o $CACHE/boost-compiled.tar.bz2
-        fi
+
+    if [ ! -e $CACHE/boost-compiled.tar.bz2 ]; then
+        echo "Downloading http://www.siusgs.com/slic3r/buildserver/boost_1_63_0.built.gcc-4.9.4-buildserver.tar.bz2 => $CACHE/boost-compiled.tar.bz2"
+        curl -L "http://www.siusgs.com/slic3r/buildserver/boost_1_63_0.built.gcc-4.9.4-buildserver.tar.bz2" -o $CACHE/boost-compiled.tar.bz2
+    fi
         
-        tar -C$HOME -xjf $CACHE/boost-compiled.tar.bz2
+    tar -C$HOME -xjf $CACHE/boost-compiled.tar.bz2
 
 elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     if [[ "$WXVERSION" != "pkg" ]]; then
