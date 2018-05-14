b9faf35b9627a76c24e349cd6358eab8b027720d
Joseph Lenox <lenox.joseph@gmail.com>
2018-05-13 22:52:01 -0500

    Fix boost download and travis

--- package/linux/travis-setup.sh
+++ package/linux/travis-setup.sh
@@ -5,19 +5,18 @@ mkdir -p $CACHE
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     if [[ "$WXVERSION"  != "pkg" ]]; then
         export WXDIR=$HOME/wx${WXVERSION}
-        if [ ! -e $CACHE/boost-compiled.tar.bz2 ]; then
-            echo "Downloading http://www.siusgs.com/slic3r/buildserver/boost_1_63_0.built.gcc-4.9.4-buildserver.tar.bz2 => $CACHE/boost-compiled.tar.bz2"
-            curl -L "http://www.siusgs.com/slic3r/buildserver/boost_1_63_0.built.gcc-4.9.4-buildserver.tar.bz2" -o $CACHE/boost-compiled.tar.bz2
-        fi
-
         if [ ! -e $CACHE/wx${WXVERSION}.tar.bz2 ]; then
             echo "Downloading http://www.siusgs.com/slic3r/buildserver/wx${WXVERSION}-libs.tar.bz2 => $CACHE/wx${WXVERSION}.tar.bz2"
             curl -L "http://www.siusgs.com/slic3r/buildserver/wx${WXVERSION}-libs.tar.bz2" -o $CACHE/wx${WXVERSION}.tar.bz2
         fi
-        
-        tar -C$HOME -xjf $CACHE/boost-compiled.tar.bz2
         tar -C$HOME -xjf $CACHE/wx${WXVERSION}.tar.bz2
     fi
+        if [ ! -e $CACHE/boost-compiled.tar.bz2 ]; then
+            echo "Downloading http://www.siusgs.com/slic3r/buildserver/boost_1_63_0.built.gcc-4.9.4-buildserver.tar.bz2 => $CACHE/boost-compiled.tar.bz2"
+            curl -L "http://www.siusgs.com/slic3r/buildserver/boost_1_63_0.built.gcc-4.9.4-buildserver.tar.bz2" -o $CACHE/boost-compiled.tar.bz2
+        fi
+        
+        tar -C$HOME -xjf $CACHE/boost-compiled.tar.bz2
 
 elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     if [[ "$WXVERSION" != "pkg" ]]; then
