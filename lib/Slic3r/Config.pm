dbf25fb7102f49fb51366dc549fe6d82d9629191
Joseph Lenox <lordofhyphens@gmail.com>
2018-05-15 17:47:28 -0500

    Hotfix Fix typo in hotfix
     (validate that % first layer heights are illegal when using adaptive slicing).

--- lib/Slic3r/Config.pm
+++ lib/Slic3r/Config.pm
@@ -169,7 +169,7 @@ sub validate {
     die "Invalid value for --first-layer-height\n"
         if $self->get_value('first_layer_height') <= 0;
     die "Adaptive slicing requires a non-relative first layer height."
-        if $self->get_value('adaptive_slicing') && $self->first_layer_height ~ /^(?:\d*(?:\.\d+)?)%$/;
+        if $self->get_value('adaptive_slicing') && $self->first_layer_height ~= /^(?:\d*(?:\.\d+)?)%$/;
     
     # --filament-diameter
     die "Invalid value for --filament-diameter\n"
