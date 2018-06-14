b32f7132b9bdd33bfe43489ee0b910484087c2f6
Joseph Lenox <lordofhyphens@gmail.com>
2018-06-14 11:41:12 -0500

    What's that semicolon?!?

--- src/GUI/Settings.cpp
+++ src/GUI/Settings.cpp
@@ -9,11 +9,11 @@ void Settings::Settings() {
     if (the_os == OS::Mac) _small_font.SetPointSize(11);
     _small_bold_font = wxSystemSettings::GetFont(wxSYS_DEFAULT_GUI_FONT);
     if (the_os == OS::Mac) _small_bold_font.SetPointSize(11);
-    _small_bold_font.MakeBold()
+    _small_bold_font.MakeBold();
     _medium_font = wxSystemSettings::GetFont(wxSYS_DEFAULT_GUI_FONT);
     _medium_font.SetPointSize(12);
 
-    _scroll_step = wxSystemSettings::GetFont(wxSYS_DEFAULT_GUI_FONT)->GetPointSize();
+    _scroll_step = wxSystemSettings::GetFont(wxSYS_DEFAULT_GUI_FONT).GetPointSize();
 }
 void Settings::save_settings() {
 /*
