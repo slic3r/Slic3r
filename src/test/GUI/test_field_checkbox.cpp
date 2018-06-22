a757796db96bbbbbe95c27daa05d504bf34243a1
Joseph Lenox <lenox.joseph@gmail.com>
2018-06-21 23:05:33 -0500

    Added a test with a tiny bit of meaning (cribbed from wxWidgets tests).

--- src/test/GUI/test_field_checkbox.cpp
+++ src/test/GUI/test_field_checkbox.cpp
@@ -7,5 +7,20 @@
 
 #include "testableframe.h"
 
-TEST_CASE( "Dummy" ) {
+SCENARIO( "checkboxes return true when checked.", "[checkbox]" ) {
+    GIVEN( "A checkbox item") {
+        wxCheckBox* m_check = new wxCheckBox(wxTheApp->GetTopWindow(), wxID_ANY, "Check box");
+        WHEN ( "the box is checked") {
+            m_check->SetValue(true);
+            THEN ( "the checkbox reads true") {
+                REQUIRE(m_check->IsChecked() == true);
+            }
+        }
+        WHEN ( "the box is not checked") {
+            m_check->SetValue(false);
+            THEN ( "the checkbox reads false") {
+                REQUIRE(m_check->IsChecked() == false);
+            }
+        }
+    }
 }
