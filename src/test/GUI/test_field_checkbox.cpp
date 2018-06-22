5bcf80790962813800c91be2fa74b3718985abed
Joseph Lenox <lenox.joseph@gmail.com>
2018-06-21 21:48:54 -0500

    Start building tests with Catch; also use ON/OFF syntax for build options.
    testableframe borrowed from wxWidgets tests.

---
+++ src/test/GUI/test_field_checkbox.cpp
@@ -0,0 +1,11 @@
+#include "test/catch.hpp"
+
+#ifndef WX_PRECOMP
+    #include "wx/app.h"
+    #include "wx/checkbox.h"
+#endif // WX_PRECOMP
+
+#include "testableframe.h"
+
+TEST_CASE( "Dummy" ) {
+}
