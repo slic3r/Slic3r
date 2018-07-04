d612bf02a40d143993af85c3945bf64814252e5e
Joseph Lenox <lenox.joseph@gmail.com>
2018-07-04 13:44:51 -0500

    C++11/14 code cleanup: NULL to nullptr for default_value options.

--- xs/src/libslic3r/ConfigBase.hpp
+++ xs/src/libslic3r/ConfigBase.hpp
@@ -640,7 +640,7 @@ class ConfigOptionDef
     /// Initialized by ConfigOptionEnum<xxx>::get_enum_values()
     t_config_enum_values enum_keys_map;
 
-    ConfigOptionDef() : type(coNone), default_value(NULL),
+    ConfigOptionDef() : type(coNone), default_value(nullptr),
                         multiline(false), full_width(false), readonly(false),
                         height(-1), width(-1), min(INT_MIN), max(INT_MAX) {};
     ConfigOptionDef(const ConfigOptionDef &other);
