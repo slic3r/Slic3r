7f7eb70dc90f4508aab68f8c0c3a6a240d6fb231
Joseph Lenox <lenox.joseph@gmail.com>
2018-07-21 08:58:02 -0500

    wrong line endings.

--- src/test/libslic3r/test_skirt_brim.cpp
+++ src/test/libslic3r/test_skirt_brim.cpp
@@ -1,5 +1,6 @@
 #include <catch.hpp>
 #include "test_data.hpp" // get access to init_print, etc
+#include "GCodeReader.hpp"
 
 using namespace Slic3r::Test;
 using namespace Slic3r;
@@ -17,8 +18,22 @@ SCENARIO("Original Slic3r Skirt/Brim tests", "[!mayfail]") {
         config->set("first_layer_speed", "100%");
 
         WHEN("multiple objects are printed") {
+            auto gcode {std::stringstream("")};
+            auto print {Slic3r::Test::init_print({TestMesh::cube_20x20x20, TestMesh::cube_20x20x20}, config)};
+            std::map<double, bool> layers_with_skirt;
+            Slic3r::Test::gcode(gcode, print);
+            auto parser {Slic3r::GCodeReader()};
+            parser.parse_stream(gcode, [&layers_with_skirt, &config] (Slic3r::GCodeReader& self, const Slic3r::GCodeReader::GCodeLine& line) 
+            {
+                if (self.Z > 0) {
+                    if (line.extruding() && line.new_F() == config->getFloat("support_material_speed") * 60.0) {
+                        layers_with_skirt[self.Z] = 1;
+                    }
+                }
+            });
+
             THEN("skirt_height is honored") {
-                REQUIRE(false);
+                REQUIRE(layers_with_skirt.size() == (size_t)config->getInt("skirt_height"));
             }
         }
     }
@@ -81,22 +96,24 @@ SCENARIO("Original Slic3r Skirt/Brim tests", "[!mayfail]") {
             config->set("cooling", 0);                     // to prevent speeds to be altered
             config->set("first_layer_speed", "100%");      // to prevent speeds to be altered
 
-            auto print {Slic3r::Test::init_print(TestMesh::overhang, config)};
+            auto print {Slic3r::Test::init_print({TestMesh::overhang}, config)};
             print.process();
             
             config->set("support_material", true);      // to prevent speeds to be altered
-            
+
             THEN("skirt length is large enough to contain object with support") {
                 REQUIRE(false);
             }
         }
         WHEN("Large minimum skirt length is used.") {
             config->set("min_skirt_length", 20);
+            auto gcode {std::stringstream("")};
             auto print {Slic3r::Test::init_print(std::make_tuple<int,int,int>(20,20,20), config)};
             THEN("Gcode generation doesn't crash") {
                 Slic3r::Test::gcode(gcode, print);
                 auto exported {gcode.str()};
                 REQUIRE(exported.size() > 0);
             }
+        }
     }
 }
