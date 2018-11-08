7dc0e6dac62b64a6d47e70d7f439285c31b83add
Alessandro Ranellucci <alessandro@pintle.it>
2018-11-08 19:04:46 +0100

    Typo

--- package/osx/travis-build-main.sh
+++ package/osx/travis-build-main.sh
@@ -8,6 +8,6 @@ brew link --overwrite perl cpanminus
 export SLIC3R_STATIC=1
 export BOOST_DIR=/usr/local
 #perl ./Build.PL
-cpanm -v --reinstall --local-lib local-lib Alien::wxwidgets
+cpanm -v --reinstall --local-lib local-lib Alien::wxWidgets
 cpanm -v --reinstall --local-lib local-lib Wx
 perl ./Build.PL --gui
