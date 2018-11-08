627e0f488a0f06cd538d1ae9ce9f5f61e440a0b7
Alessandro Ranellucci <alessandro@pintle.it>
2018-11-08 18:17:35 +0100

    Enable verbose output for cpanm Wx

--- package/osx/travis-build-main.sh
+++ package/osx/travis-build-main.sh
@@ -7,5 +7,6 @@ brew link --overwrite perl cpanminus
 
 export SLIC3R_STATIC=1
 export BOOST_DIR=/usr/local
-perl ./Build.PL
+#perl ./Build.PL
+cpanm -v --local-lib local-lib Wx
 perl ./Build.PL --gui
