cbb618a381c3f7cdea95bb400782cc510b790e86
Alessandro Ranellucci <aar@cpan.org>
2018-11-08 13:51:46 +0100

    More tweaks for Travis caching

--- package/linux/travis-build-main.sh
+++ package/linux/travis-build-main.sh
@@ -9,13 +9,16 @@ if [ ! -d $HOME/perl5/perlbrew/perls/slic3r-perl ]; then
     tar -C$HOME/perl5/perlbrew/perls -xjf /tmp/slic3r-perlbrew-5.24.tar.bz2
 fi
 
+source $HOME/perl5/perlbrew/etc/bashrc
+perlbrew switch slic3r-perl
+
 if [ ! -e $HOME/boost_1_63_0/boost/version.hpp ]; then
     echo "Downloading boost_1_63_0.built.gcc-4.9.4-buildserver.tar.bz2"
     curl -L "http://www.siusgs.com/slic3r/buildserver/boost_1_63_0.built.gcc-4.9.4-buildserver.tar.bz2" -o /tmp/boost-compiled.tar.bz2
     tar -C$HOME -xjf /tmp/boost-compiled.tar.bz2
 fi
 
-if !(perl -MWx -e1 2> /dev/null); then
+if [ ! -e ./local-lib/lib/perl5/x86_64-linux-thread-multi/Wx.pm ]; then
     echo "Downloading slic3r-dependencies.gcc49.travis-wx302.tar.bz2"
     curl -L "http://www.siusgs.com/slic3r/buildserver/slic3r-dependencies.travis-wx302.tar.bz2" -o /tmp/local-lib-wx302.tar.bz2
     tar -C$TRAVIS_BUILD_DIR -xjf /tmp/local-lib-wx302.tar.bz2
@@ -25,7 +28,5 @@ if !(perl -MWx -e1 2> /dev/null); then
     tar -C$HOME -xjf /tmp/wx302.tar.bz2
 fi
 
-source $HOME/perl5/perlbrew/etc/bashrc
-perlbrew switch slic3r-perl
-
 SLIC3R_STATIC=1 CC=g++-4.9 CXX=g++-4.9 BOOST_DIR=$HOME/boost_1_63_0 perl ./Build.PL
+perl ./Build.PL --gui
