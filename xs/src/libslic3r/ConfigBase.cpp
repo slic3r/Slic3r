46602dca783c98a29220b0b43db00f3f997f39b5
Alessandro Ranellucci <aar@cpan.org>
2018-11-21 23:28:57 +0100

    Fix wrong brackets (They were compiling on clang!)

--- xs/src/libslic3r/ConfigBase.cpp
+++ xs/src/libslic3r/ConfigBase.cpp
@@ -418,11 +418,11 @@ ConfigBase::set_deserialize(t_config_option_key opt_key, std::string str, bool a
             }
         }
     }
-    if (!this->def->has(opt_key)) {
+    if (!this->def->has(opt_key))
         throw UnknownOptionException(opt_key);
     
     const ConfigOptionDef& optdef = this->def->options.at(opt_key);
-    if (!optdef.shortcut.empty())
+    if (!optdef.shortcut.empty()) {
         for (const t_config_option_key& shortcut : optdef.shortcut)
             if (!this->set_deserialize(shortcut, str))
                 return false;
