8ba258be98e4667aa2604b8f65f8bf6828fd1d6f
Joseph Lenox <lenox.joseph@gmail.com>
2019-01-06 11:49:16 -0600

    Update windows build server command to clean up old branch builds.

--- package/deploy/winscp.ps1
+++ package/deploy/winscp.ps1
@@ -12,6 +12,12 @@ Set-Variable -Name "UUSER" -Value "$env:UPLOAD_USER"
 Set-Variable -Name "UPLOAD" -Value "$($FILE | Resolve-Path)"
 if (Test-Path $KEY) {
     if ($env:APPVEYOR_PULL_REQUEST_NUMBER -Or $env:APPVEYOR_REPO_BRANCH -ne "master" ) {
+        if ($env:APPVEYOR_PULL_REQUEST_NUMBER) {
+            winscp.com /privatekey=$KEY /command "open sftp://$UUSER@dl.slic3r.org/$DIR/branches -hostkey=*" "rm *PR${env:APPVEYOR_PULL_REQUEST_NUMBER}*" "exit"
+        }
+        if ($env:APPVEYOR_REPO_BRANCH -ne "master" ) {
+            winscp.com /privatekey=$KEY /command "open sftp://$UUSER@dl.slic3r.org/$DIR/branches -hostkey=*" "rm *${env:APPVEYOR_REPO_BRANCH}*" "exit"
+        }
         winscp.com /privatekey=$KEY /command "open sftp://$UUSER@dl.slic3r.org/$DIR/branches -hostkey=*" "put $UPLOAD ./$FILE" "exit"
     } else {
         winscp.com /privatekey=$KEY /command "open sftp://$UUSER@dl.slic3r.org/$DIR -hostkey=*" "put $UPLOAD ./$FILE" "exit"
