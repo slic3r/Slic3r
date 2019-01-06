2e127c23fbe77f663a97333ec0450293538aa26c
Joseph Lenox <lenox.joseph@gmail.com>
2019-01-05 18:27:07 -0600

    Correct logic for min/max layer height.

--- lib/Slic3r/Config.pm
+++ lib/Slic3r/Config.pm
@@ -272,7 +272,7 @@ sub validate {
     }
     # --min-layer-height and --max-layer-height
         die "Max layer height should be greater than min layer height."
-            if !($self->max_layer_height > $self->min_layer_height);
+            if $self->adaptive_slicing && ($self->max_layer_height < $self->min_layer_height);
     
     # extrusion widths
     {
