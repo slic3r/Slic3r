364f2a6e8cd2e10678f40abfcb2a637bb285ec9e
Joseph Lenox <lordofhyphens@gmail.com>
2019-01-06 18:03:24 -0600

    Patch for #4620 - ensure posDetectSurfaces is invalidated on slice change (#4679)
    * Update some language used 
    
    * Completely check pre-condition in test. 
    
    Make sure that we have 1 solid bottom layer and 2 solid top layers before invoking our change.
    
    * Use step_invalidate() instead to ensure dependencies are invalidated as well.
    
    Move set_started() until after invalidations.

--- src/test/libslic3r/test_print.cpp
+++ src/test/libslic3r/test_print.cpp
@@ -86,12 +86,16 @@ SCENARIO("Print: Changing number of solid surfaces does not cause all surfaces t
         std::string stage;
         auto print {Slic3r::Test::init_print({m}, model, config)};
         print->process();
-        test_is_solid_infill(print, 0, 39); // check to make sure it sliced properly
+        // Precondition: Ensure that the model has 2 solid top layers (39, 38)
+        // and one solid bottom layer (0).
+        test_is_solid_infill(print, 0, 0); // should be solid
+        test_is_solid_infill(print, 0, 39); // should be solid
+        test_is_solid_infill(print, 0, 38); // should be solid
         WHEN("Model is re-sliced with top_solid_layers == 3") {
             print->regions[0]->config.top_solid_layers = 3;
             print->objects[0]->invalidate_step(posPrepareInfill);
             print->process();
-            THEN("Print object does not have 0 bottom surfaces.") {
+            THEN("Print object does not have 0 solid bottom layers.") {
                 test_is_solid_infill(print, 0, 0);
             }
             AND_THEN("Print object has 3 top solid layers") {
