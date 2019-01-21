70946af2ae0fa0be69b16defbe14ce72d71071e4
J-P Nurmi <jpnurmi@gmail.com>
2019-01-21 21:03:51 +0100

    Add missing quotes to startup scripts

--- package/linux/startup_script.sh
+++ package/linux/startup_script.sh
@@ -3,4 +3,4 @@
 BIN=$(readlink -f "$0")
 DIR=$(dirname "$BIN")
 export LD_LIBRARY_PATH="$DIR/bin"
-exec "$DIR/perl-local" -I"$DIR/local-lib/lib/perl5" "$DIR/slic3r.pl" $@
+exec "$DIR/perl-local" -I"$DIR/local-lib/lib/perl5" "$DIR/slic3r.pl" "$@"
