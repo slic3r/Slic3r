8dc4e64160deae26407a45289f16f99d082d1e58
3bhady <mohamed_abdelhady96@hotmail.com>
2019-04-03 05:02:36 +0200

    Testing 3mf reading file with umlauts in it's path

---
+++ src/test/libslic3r/test_3mf.cpp
@@ -0,0 +1,20 @@
+#include <catch.hpp>
+#include <test_options.hpp>
+#include <string>
+#include "Model.hpp"
+#include "TMF.hpp"
+
+
+using namespace Slic3r;
+
+SCENARIO("Reading 3mf file") {
+    GIVEN("umlauts in the path of the file") {
+         auto model {new Slic3r::Model()};
+        WHEN("3mf model is read") {
+            auto ret = Slic3r::IO::TMF::read(std::string(testfile_dir) +"test_3mf/Geräte/box.3mf",model);
+            THEN("read should not return 0") {
+                REQUIRE(ret == 1);
+            }
+        }
+    }
+}
