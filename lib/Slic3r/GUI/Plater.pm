b69a9015a9379ff2a6b45be361b00dd9f820ef89
Sergey Alirzaev <zl29ah@gmail.com>
2019-05-01 06:37:55 +0300

    Show estimated printing time after exporting gcode
    fixes https://github.com/slic3r/Slic3r/issues/4758

--- lib/Slic3r/GUI/Plater.pm
+++ lib/Slic3r/GUI/Plater.pm
@@ -599,6 +599,7 @@ sub new {
             my @info = (
                 fil     => "Used Filament",
                 cost    => "Cost",
+                tim     => "Time",
             );
             while (my $field = shift @info) {
                 my $label = shift @info;
@@ -2281,6 +2282,17 @@ sub on_export_completed {
         }
     } else {
         $message = "Export failed";
+    }
+    # Estimate print duration
+    {
+        my $estimator = Slic3r::GCode::TimeEstimator->new;
+        $estimator->parse_file($self->{export_gcode_output_file});
+        my $time = $estimator->time;
+        $self->{print_info_tim}->SetLabel(sprintf(
+            "%d minutes and %d seconds",
+            int($time / 60),
+            int($time % 60),
+        ));
     }
     $self->{export_gcode_output_file} = undef;
     $self->statusbar->SetStatusText($message);
