3d3f71c26dd4f2863fa58206d58c7211aa022119
Michael Kirsch <mkirsch.git@gmail.com>
2019-05-28 21:12:29 +0200

    fix include define to align name

--- xs/src/libslic3r/TransformationMatrix.hpp
+++ xs/src/libslic3r/TransformationMatrix.hpp
@@ -1,5 +1,5 @@
-#ifndef slic3r_TriangleMatrix_hpp_
-#define slic3r_TriangleMatrix_hpp_
+#ifndef slic3r_TransformationMatrix_hpp_
+#define slic3r_TransformationMatrix_hpp_
 
 #include "libslic3r.h"
 #include "Point.hpp"
