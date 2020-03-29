d82e41cf5abf2101d0259613b43b44a5d6adac67
Roman Dvorak <romandvorak@mlab.cz>
2020-03-29 21:14:23 +0200

    Base path

--- package/deploy/sftp.sh
+++ package/deploy/sftp.sh
@@ -13,24 +13,28 @@ source $(dirname $0)/../common/util.sh
 set_pr_id
 set_branch
 if [ ! -z ${PR_ID+x} ] || [ $current_branch != "master" ]; then
-    DIR=${DIR}/branches
+    DIR=${BASE_DIR}${DIR}/branches
 fi
 
 if [ -s $KEY ]; then
     if [ ! -z ${PR_ID+x} ] || [ $current_branch != "master" ]; then
+        echo "AAA"
         # clean up old copies of the same branch/PR
         if [ ! -z ${PR_ID+x} ]; then
+            echo "BBB"
             echo "rm *PR${PR_ID}*" | sftp -i$KEY -P 2222 "${UPLOAD_USER}@s1.mlab.cz:$DIR/"
         fi
         if [ $current_branch != "master" ]; then
+            echo "CCC"
             echo "rm *${current_branch}*" | sftp -i$KEY -P 2222 "${UPLOAD_USER}@s1.mlab.cz:$DIR/"
         fi
     fi
     for i in $FILES; do
+        echo "DDD"
         filepath=$i  # this is expected to be an absolute path
         tmpfile=$(mktemp)
         echo put $filepath > $tmpfile
-
+	echo $tmpfile
         sftp -b $tmpfile -i$KEY -P 2222 "${UPLOAD_USER}@s1.mlab.cz:$DIR/"
         result=$?
         if [ $? -eq 1 ]; then
