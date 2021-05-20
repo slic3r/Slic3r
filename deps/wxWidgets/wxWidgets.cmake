f0d9be8a78d8c5c7a0e4318d1a7b2527a636ea2b
remi durand <remi-j.durand@thalesgroup.com>
2021-05-20 11:18:32 +0200

    fix debug builds

--- deps/wxWidgets/wxWidgets.cmake
+++ deps/wxWidgets/wxWidgets.cmake
@@ -26,7 +26,7 @@ prusaslicer_add_cmake_project(wxWidgets
         -DwxBUILD_PRECOMP=ON
         ${_wx_toolkit}
         "-DCMAKE_DEBUG_POSTFIX:STRING="
-        -DwxBUILD_DEBUG_LEVEL=0
+        -DwxBUILD_DEBUG_LEVEL=$<$<CONFIG:Debug>:1>$<$<NOT:$<CONFIG:Debug>>:0>
         -DwxUSE_DETECT_SM=OFF
         -DwxUSE_UNICODE=ON
         -DwxUSE_OPENGL=ON
