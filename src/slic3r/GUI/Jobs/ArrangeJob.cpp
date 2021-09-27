f8ca156bf5cfef745cf4ddac2ef1470ef3fbd6b1
supermerill <merill@free.fr>
2021-09-27 13:16:38 +0200

    add include for archlinux supermerill/SuperSlicer#703

--- src/slic3r/GUI/Jobs/ArrangeJob.cpp
+++ src/slic3r/GUI/Jobs/ArrangeJob.cpp
@@ -3,6 +3,7 @@
 #include "libslic3r/MTUtils.hpp"
 #include "libslic3r/Model.hpp"
 #include "libslic3r/Print.hpp"
+#include "libslic3r/ClipperUtils.hpp"
 
 #include "slic3r/GUI/Plater.hpp"
 #include "slic3r/GUI/GLCanvas3D.hpp"
